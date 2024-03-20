a87ba74f0d82b2449315482e4a88336bdc263028
gabriel-rigaud <gabriel.rigaud@laplateforme.io>
2024-03-20 13:03:47 +0100

    up

---
+++ vente.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vente de produits</title>
+</head>
+<body>
+<header>
+    <?php include 'header.php'?>
+</header>
+
+<h1>Produits à vendre</h1>
+<div class="mosaique">
+    <div class="produit">
+        <img src="https://www.auparadisduthe.com/blog/wp-content/uploads/2020/12/sachet-de-the%CC%81-en-nylon-pyramidal.jpg" alt="Produit 1">
+        <h2>Matcha Premium</h2>
+        <p>Matcha Premium: Un thé vert japonais moulu en une fine poudre, réputé pour sa couleur vibrante, son goût riche et sa texture veloutée. Le matcha premium est prisé pour ses qualités antioxydantes et son énergie durable.</p>
+        <p>Prix : <span class="prix">12€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://kodamaparis.com/cdn/shop/articles/Sachet_de_the_1400x.png?v=1601370413" alt="Produit 2">
+        <h2>Thé vert Sencha</h2>
+        <p>Thé vert Sencha: Originaire du Japon, le Sencha est un thé vert aux feuilles longues et fines, offrant une saveur fraîche et végétale avec une légère douceur. Il est apprécié pour sa teneur élevée en antioxydants et sa fraîcheur revigorante.</p>
+        <p>Prix : <span class="prix">16€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/8/80/Tea_bags.jpg" alt="Produit 3">
+        <h2>Thé noir Assam</h2>
+        <p>Thé noir Assam: Originaire de la région d'Assam en Inde, ce thé noir est connu pour son corps robuste, sa couleur cuivrée et ses notes maltées prononcées. C'est un thé idéal pour ceux qui recherchent une infusion puissante et revigorante.</p>
+        <p>Prix : <span class="prix">25€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLvTMj2oW68LnlOe0WFRLsD3oYcItAA-y4uEC94fS-yg&s" alt="Produit 4">
+        <h2>Thé blanc Pai Mu Tan</h2>
+        <p>Thé blanc Pai Mu Tan: Un thé blanc délicat et raffiné, composé de bourgeons argentés et de jeunes feuilles. Il offre une saveur douce et florale avec des notes subtiles de melon. Le Pai Mu Tan est apprécié pour sa douceur et ses bienfaits pour la santé.</p>
+        <p>Prix : <span class="prix">35€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://www.mieux-vivre-autrement.com/wp-content/uploads/2017/09/shutterstock_358560767-900x514.jpg" alt="Produit 5">
+        <h2>Thé oolong Dong Ding</h2>
+        <p>Thé oolong Dong Ding: Originaire de Taiwan, le Dong Ding est un thé oolong semi-oxydé avec des notes florales et fruitées, complétées par une légère touche de torréfaction. Il offre une expérience complexe et équilibrée pour les amateurs de thé.</p>
+        <p>Prix : <span class="prix">55€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://smilepack.fr/files/subscribers/d72b0b53-b3d0-49ff-adfd-37d51546ebde/sites/c8ba1de3-3336-4693-8817-239caefd438c/products/ad10c6b0-5f5f-4ffc-9b4d-f08e5b7f74b7/smilepack-packaging-cafe-theboite-24-sachets-the-7_xlarge.png?stamp=638086319052514919" alt="Produit 6">
+        <h2>Thé vert Genmaicha</h2>
+        <p>Thé vert Genmaicha: Un thé japonais unique combinant du thé vert Sencha avec des grains de riz soufflés, offrant une saveur légèrement grillée et un arôme de riz torréfié. Le Genmaicha est apprécié pour son caractère réconfortant et sa douceur.</p>
+        <p>Prix : <span class="prix">78€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5GB8As7xIs4p4rqy6cuzCa1cL2eooVl710L8noZRw0A&s" alt="Produit 7">
+        <h2>Thé vert Gyokuro</h2>
+        <p>Thé vert Gyokuro: Un thé vert japonais haut de gamme cultivé à l'ombre, offrant une infusion douce et sucrée avec des notes d'algues marines. Le Gyokuro est réputé pour sa couleur vert émeraude et son goût délicat.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://m.media-amazon.com/images/I/71ZnT9g+dmL._AC_UF1000,1000_QL80_.jpg" alt="Produit 8">
+        <h2>Thé noir Earl Grey</h2>
+        <p>Un thé noir aromatisé à la bergamote, offrant une combinaison unique de saveur corsée et d'agrumes rafraîchissants. L'Earl Grey est un classique intemporel apprécié pour son arôme parfumé et son goût élégant.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://blog.petit-mariage-entre-amis.fr/blog/wp-content/uploads/2021/11/sachet-the-cadeau-invite-mariage.jpg" alt="Produit 9">
+        <h2>Thé blanc Silver Needle</h2>
+        <p>Un thé blanc haut de gamme composé uniquement de bourgeons argentés, offrant une infusion délicate et raffinée avec des notes florales douces. Le Silver Needle est apprécié pour sa pureté et sa douceur exquise.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://www.lebeauthe.com/cdn/shop/products/sachetsdethepersonnalisesmariage_1024x.jpg?v=1612954537" alt="Produit 10">
+        <h2>Thé oolong Tie Guan Yin</h2>
+        <p> Un thé oolong chinois semi-oxydé avec des notes florales et beurrées, offrant une infusion douce et crémeuse avec une finale légèrement sucrée. Le Tie Guan Yin est apprécié pour sa complexité et sa texture veloutée.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://letempsdesenvies.com/6563-large_default/boite-15-sachets-de-the-coeur-xoxo_tea-heritage.jpg" alt="Produit 11">
+        <h2>Thé vert Jasmine Pearls</h2>
+        <p> Un thé vert parfumé aux fleurs de jasmin, roulé en petites perles délicates qui se déploient lors de l'infusion, libérant un arôme floral enivrant et une saveur douce et rafraîchissante.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://cdn.shopify.com/s/files/1/0263/9387/0398/files/sachet-de-the-le-beau-the_480x480.jpg?v=1602591238" alt="Produit 12">
+        <h2>Thé noir Darjeeling</h2>
+        <p>Thé noir Darjeeling: Un thé noir élégant originaire des collines de Darjeeling en Inde, offrant une infusion légère et lumineuse avec des notes florales et fruitées. Le Darjeeling est apprécié pour sa subtilité et sa complexité.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://m.media-amazon.com/images/I/41vRYD6GfRL._AC_UF1000,1000_QL80_.jpg" alt="Produit 13">
+        <h2>Thé blanc White Peony</h2>
+        <p>Thé blanc White Peony: Un thé blanc composé de bourgeons et de jeunes feuilles, offrant une infusion douce et soyeuse avec des notes florales et des nuances de miel. Le White Peony est apprécié pour sa fraîcheur et sa douceur naturelle.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxyC_isTZkK4dgYvXc0KwP8_fhaHEUL9VYs2i_u85zSA&s" alt="Produit 14">
+        <h2>Thé oolong Da Hong Pao</h2>
+        <p>Thé oolong Da Hong Pao: Un thé oolong chinois légendaire cultivé dans les montagnes de la province de Fujian, offrant une infusion riche et complexe avec des notes de roche minérale, de bois précieux et de fruits mûrs.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+    <div class="produit">
+        <img src="https://cherry.img.pmdstatic.net/fit/https.3A.2F.2Fimg.2Eohmymag.2Ecom.2Farticle.2Fzero-dechet.2F5-astuces-avec-un-sachet-de-the_30424e526530b8f9d445c8f2af75bdd461711b72.2Ejpg/1200x675/quality/80/5-astuces-avec-un-sachet-de-the.jpg" alt="Produit 15">
+        <h2>Thé vert Chun Mee</h2>
+        <p>Thé vert Chun Mee: Un thé vert chinois aux feuilles fines et recourbées, offrant une infusion corsée et rafraîchissante avec des notes herbacées et une légère astringence. Le Chun Mee est apprécié pour son caractère stimulant et sa fraîcheur revigorante.</p>
+        <p>Prix : <span class="prix">80€</span></p>
+        <button class="ajt-panier">Ajouté au panier</button>
+    </div>
+
+
+</div>
+
+<footer>
+    <?php include 'footer.php'?>
+</footer>
+</body>
+</html>
