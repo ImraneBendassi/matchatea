f66d1b5ed7a71cbebe9dfc463c05b9b2b894aab7
gabriel-rigaud <114390299+gabriel-rigaud@users.noreply.github.com>
2024-03-25 09:20:52 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="style-gabriel.css">
+
+
+<nav>
+    <ul>
+        <li><a href="histoire.php">Histoire</a></li>
+        <li><a href="vente.php">Vente</a></li>
+    </ul>
+</nav>
