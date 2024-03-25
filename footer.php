669a7e1abad77108670d1b2d294e2903add4a45f
gabriel-rigaud <gabriel.rigaud@laplateforme.io>
2024-03-25 09:17:16 +0100

    del

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>&copy; 2024 Matcha Tea. Tous droits réservés.</p>
-</footer>
--- header.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="style.css">
-
-
-<nav>
-    <ul>
-        <li><a href="histoire.php">Histoire</a></li>
-        <li><a href="vente.php">Vente</a></li>
-    </ul>
-</nav>
